c648ffc9183c501f9a3266c4fd00e336d8302ace
Florian Roth <venom14@gmail.com>
2016-04-30 16:58:13 +0200

    v0.15.5
    - Fixed python crash bug when passing unicode strings to YARA’s match
    function

--- loki.py
+++ loki.py
@@ -24,7 +24,7 @@ BSK Consulting GmbH
 
 DISCLAIMER - USE AT YOUR OWN RISK.
 """
-__version__ = '0.15.3'
+__version__ = '0.15.5'
 
 import os
 import argparse
@@ -327,7 +327,8 @@ class Loki():
                             # Scan the read data
                             try:
                                 for (score, rule, description, matched_strings) in \
-                                    self.scan_data(fileData, fileType, filename, filePath, extension, md5):
+                                        self.scan_data(fileData, fileType, removeNonAsciiDrop(filename),
+                                                       removeNonAscii(filePath), extension, md5):
 
                                     # Message
                                     message = "Yara Rule MATCH: %s TYPE: %s DESCRIPTION: %s FILE: %s FIRST_BYTES: %s %s " \
@@ -423,7 +424,7 @@ class Loki():
         windll = ctypes.windll.kernel32
         locale = locale.windows_locale[ windll.GetUserDefaultUILanguage() ]
         if locale == 'fr_FR':
-            return (owner.upper().startswith("SERVICE LOCAL") or 
+            return (owner.upper().startswith("SERVICE LOCAL") or
                 owner.upper().startswith(u"SERVICE RÃSEAU") or
 #                owner.upper().startswith(u"SystÃ¨me") or ##Not matching
                 owner == u"SystÃ¨me" or
@@ -1234,7 +1235,7 @@ if __name__ == '__main__':
     # Remove old log file
     if os.path.exists(args.l):
         os.remove(args.l)
-	
+
     # Computername
     if platform == "linux" or platform == "osx":
         t_hostname = os.uname()[1]
