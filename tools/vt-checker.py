910a30a7176a1382db7c810ee7b4cfb8c97ef011
Florian Roth <florian.roth@bsk-consulting.de>
2016-10-17 15:43:54 +0200

    Minor Improvements
    - Include all hashes in CSV

--- tools/vt-checker.py
+++ tools/vt-checker.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python2.7
-"""Checks Hashes read from an input file on Virustotal""" 
+"""Checks Hashes read from an input file on Virustotal"""
 
 __AUTHOR__ = 'Florian Roth'
-__VERSION__ = "0.3 July 2016"
+__VERSION__ = "0.4 October 2016"
 
 """
 Install dependencies with:
@@ -26,7 +26,7 @@ from colorama import init, Fore, Back, Style
 URL = r'https://www.virustotal.com/vtapi/v2/file/report'
 VENDORS = ['Microsoft', 'Kaspersky', 'McAfee']
 API_KEY = ''
-WAIT_TIME = 15 # Public API allows 4 request per minute, so we wait 15 secs by default 
+WAIT_TIME = 15  # Public API allows 4 request per minute, so we wait 15 secs by default
 
 
 def fetch_hash(line):
@@ -45,17 +45,17 @@ def print_highlighted(line, hl_color=Back.WHITE):
     Print a highlighted line
     """
     colorer = re.compile('([A-Z_]{2,}:)\s', re.VERBOSE)
-    line = colorer.sub(Fore.BLACK+hl_color+r'\1'+Style.RESET_ALL+' ', line)
+    line = colorer.sub(Fore.BLACK + hl_color + r'\1' + Style.RESET_ALL + ' ', line)
     print line
 
 
 def process_permalink(url, debug=False):
     """
-    Requests the HTML page for the sample and extracts other useful data 
-    that is not included in the public API 
+    Requests the HTML page for the sample and extracts other useful data
+    that is not included in the public API
     """
-    headers = {'User-Agent' : 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
-               'Referrer': 'https://www.virustotal.com/en/' }
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+               'Referrer': 'https://www.virustotal.com/en/'}
     request = urllib2.Request(url, None, headers)
     info = {'filenames': ['-'], 'firstsubmission': '-'}
     try:
@@ -68,10 +68,10 @@ def process_permalink(url, debug=False):
         for i, row in enumerate(elements):
             text = row.text.strip()
             if text == "File names":
-                file_names = elements[i+1].text.strip().split("\n")
+                file_names = elements[i + 1].text.strip().split("\n")
                 info['filenames'] = filter(None, map(lambda file: file.strip(), file_names))
         # Get first submission
-        elements = soup.findAll("div", { "class" : "enum" })
+        elements = soup.findAll("div", {"class": "enum"})
         for i, row in enumerate(elements):
             text = row.text.strip()
             if 'First submission' in text:
@@ -138,11 +138,11 @@ def process_lines(lines, result_file, nocsv=False, dups=False, debug=False):
         # Skip comments
         if line.startswith("#"):
             continue
-        
+
         # Remove line break
         line.rstrip("\n\r\l")
 
-        # Get all hashes in line 
+        # Get all hashes in line
         # ... and the rest of the line as comment
         hash, comment = fetch_hash(line)
 
@@ -153,12 +153,12 @@ def process_lines(lines, result_file, nocsv=False, dups=False, debug=False):
         # Cache
         if hash in cache:
             if dups:
-                # Colorized head of each hash check 
+                # Colorized head of each hash check
                 print_highlighted("\nHASH: {0} COMMENT: {1}".format(hash, comment))
                 print_highlighted("RESULT: %s (from cache)" % cache[hash])
             continue
         else:
-            # Colorized head of each hash check 
+            # Colorized head of each hash check
             print_highlighted("\nHASH: {0} COMMENT: {1}".format(hash, comment))
 
         # Prepare VT API request
@@ -177,7 +177,7 @@ def process_lines(lines, result_file, nocsv=False, dups=False, debug=False):
                     traceback.print_exc()
                     # print "Error requesting VT results"
                 pass
-        
+
         # Process results
         result = "- / -"
         virus = "-"
@@ -187,19 +187,26 @@ def process_lines(lines, result_file, nocsv=False, dups=False, debug=False):
         rating = "unknown"
         positives = 0
         res_color = Back.CYAN
+        md5 = "-"
+        sha1 = "-"
+        sha256 = "-"
         # print simplejson.dumps(response_dict, sort_keys=True, indent=4)
         if response_dict.get("response_code") > 0:
+            # Hashes
+            md5 = response_dict.get("md5")
+            sha1 = response_dict.get("sha1")
+            sha256 = response_dict.get("sha256")
             # AV matches
             positives = response_dict.get("positives")
             total = response_dict.get("total")
             last_submitted = response_dict.get("scan_date")
-            # Virus Name 
+            # Virus Name
             scans = response_dict.get("scans")
             virus_names = []
             for vendor in VENDORS:
                 if vendor in scans:
                     if scans[vendor]["result"]:
-                        virus_names.append(scans[vendor]["result"])
+                        virus_names.append("{0}: {1}".format(vendor, scans[vendor]["result"]))
             if len(virus_names) > 0:
                 virus = " / ".join(virus_names)
             # Type
@@ -218,18 +225,21 @@ def process_lines(lines, result_file, nocsv=False, dups=False, debug=False):
             # File Names
             filenames = removeNonAsciiDrop(", ".join(info['filenames'][:5]).replace(';', '_'))
             first_submitted = info['firstsubmission']
-            # Result 
-            result = "%s / %s" % ( response_dict.get("positives"), response_dict.get("total") )
+            # Result
+            result = "%s / %s" % (response_dict.get("positives"), response_dict.get("total"))
             print_highlighted("VIRUS: {0}".format(virus))
             print_highlighted("FILENAMES: {0}".format(filenames))
             print_highlighted("FIRST_SUBMITTED: {0} LAST_SUBMITTED: {1}".format(first_submitted, last_submitted))
-        
+
         # Print the highlighted result line
         print_highlighted("RESULT: %s" % result, hl_color=res_color)
 
         # Add to log file
         if not nocsv:
-            result_line = "{0};{1};{2};{3};{4};{5};{6};{7}\n".format(hash, rating, comment, positives, virus, filenames, first_submitted, last_submitted)
+            result_line = "{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10}\n".format(hash, rating, comment, positives,
+                                                                                  virus, filenames, first_submitted,
+                                                                                  last_submitted,
+                                                                                  md5, sha1, sha256)
             with open(result_file, "a") as fh_results:
                 fh_results.write(result_line)
 
@@ -238,6 +248,7 @@ def process_lines(lines, result_file, nocsv=False, dups=False, debug=False):
 
         # Wait some time for the next request
         time.sleep(WAIT_TIME)
+
 
 if __name__ == '__main__':
 
@@ -259,12 +270,12 @@ if __name__ == '__main__':
                         default='vt-hash-db.pkl')
     parser.add_argument('--nocache', action='store_true', help='Do not use cache database file', default=False)
     parser.add_argument('--nocsv', action='store_true', help='Do not write a CSV with the results', default=False)
-    parser.add_argument('--dups', action='store_true', help='Do not skip duplicate hashes', default=False) 
+    parser.add_argument('--dups', action='store_true', help='Do not skip duplicate hashes', default=False)
     parser.add_argument('--debug', action='store_true', default=False, help='Debug output')
 
     args = parser.parse_args()
 
-    # Check API Key 
+    # Check API Key
     if API_KEY == '':
         print "[E] No API Key set"
         print "    Include your API key in the header section of the script (API_KEY)\n"
@@ -292,7 +303,7 @@ if __name__ == '__main__':
             print "[-] No cache database found"
             print "[+] Analyzed hashes will be written to cache database: {0}".format(args.c)
         print "[+] You can always interrupt the scan by pressing CTRL+C without loosing the scan state"
-    
+
     # Open input file
     try:
         with open(args.f, 'r') as fh:
@@ -312,7 +323,8 @@ if __name__ == '__main__':
             print "[+] Writing results to new file: {0}".format(result_file)
             try:
                 with open(result_file, 'w') as fh_results:
-                    fh_results.write("Hash;Rating;Comment;Positives;Virus;File Names;First Submitted;Last Submitted\n")
+                    fh_results.write("Lookup Hash;Rating;Comment;Positives;Virus;File Names;First Submitted;"
+                                     "Last Submitted;MD5;SHA1;SHA256\n")
             except Exception, e:
                 print "[E] Cannot write export file {0}".format(result_file)
 
@@ -324,3 +336,4 @@ if __name__ == '__main__':
     saveCache(cache, args.c)
 
     print Style.RESET_ALL
+
