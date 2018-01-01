c91d508a23a102580b8371c3189ee342dc0b4901
Florian Roth <florian.roth@bsk-consulting.de>
2018-01-01 21:45:34 +0100

    Run Process helper

--- lib/helpers.py
+++ lib/helpers.py
@@ -24,6 +24,9 @@ import netaddr
 import datetime
 import platform
 import time
+import threading
+import subprocess
+import signal
 
 # Helper Functions -------------------------------------------------------------
 
@@ -272,3 +275,38 @@ def getAgeString(filePath):
     except Exception as e:
         timestring = "CREATED: not_available MODIFIED: not_available ACCESSED: not_available"
     return timestring
+
+
+def runProcess(command, timeout=10):
+    """
+    Run a process and check it's output
+    :param command:
+    :return output:
+    """
+    output = ""
+    returnCode = 0
+
+    # Kill check
+    kill_check = threading.Event()
+    def _kill_process_after_a_timeout(pid):
+        os.kill(pid, signal.SIGTERM)
+        kill_check.set() # tell the main routine that we had to kill
+        print("timeout hit - killing pid {0}".format(pid))
+        # use SIGKILL if hard to kill...
+        return "", 1
+    try:
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        returnCode = e.returncode
+        traceback.print_exc()
+    #print p.communicate()[0]
+    pid = p.pid
+    watchdog = threading.Timer(timeout, _kill_process_after_a_timeout, args=(pid, ))
+    watchdog.start()
+    (stdout, stderr) = p.communicate()
+    output = "{0} {1}".format(stdout, stderr)
+    watchdog.cancel() # if it's still waiting to run
+    success = not kill_check.isSet()
+    kill_check.clear()
+
+    return output, returnCode
