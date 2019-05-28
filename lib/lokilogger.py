8ae2ff7b03cba225cf9a4e00921fb4aaf680a392
Florian Roth <florian.roth@bsk-consulting.de>
2019-05-28 11:17:37 +0200

    docs: version number increased

--- lib/lokilogger.py
+++ lib/lokilogger.py
@@ -14,7 +14,7 @@ import logging.handlers
 import socket
 from helpers import removeNonAsciiDrop
 
-__version__ = '0.29.2'
+__version__ = '0.29.3'
 
 # Logger Class -----------------------------------------------------------------
 class LokiLogger():
