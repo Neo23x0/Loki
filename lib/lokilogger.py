b36f06b5c412bb4a62ac5f1e82356bd71e14cf04
Florian Roth <florian.roth@bsk-consulting.de>
2019-08-29 18:38:01 +0200

    refactor: additional debug output

--- lib/lokilogger.py
+++ lib/lokilogger.py
@@ -14,7 +14,7 @@ from logging import handlers
 import socket
 from helpers import removeNonAsciiDrop
 
-__version__ = '0.30.2'
+__version__ = '0.30.3'
 
 # Logger Class -----------------------------------------------------------------
 class LokiLogger():
