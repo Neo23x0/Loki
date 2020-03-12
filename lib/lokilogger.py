ce878cfc4864c60fef93b549ff1d6e90befde940
Florian Roth <florian.roth@bsk-consulting.de>
2020-03-12 14:07:04 +0100

    chore: upgrade to PE-Sieve 0.2.5

--- lib/lokilogger.py
+++ lib/lokilogger.py
@@ -14,7 +14,7 @@ from logging import handlers
 import socket
 from helpers import removeNonAsciiDrop
 
-__version__ = '0.30.5'
+__version__ = '0.30.6'
 
 # Logger Class -----------------------------------------------------------------
 class LokiLogger():
