2dbcadb6beff484024fe9a36a01b03603e8ca56c
Florian Roth <florian.roth@nextron-systems.com>
2021-03-04 15:09:17 +0100

    feat: new build scripts

--- build.bat
+++ build.bat
@@ -1,68 +1,40 @@
 @ECHO OFF
-:: Build script by A. Fischer and F. Roth
-:: November 2017
+REM Building LOKI
 
 setlocal enabledelayedexpansion
 
-if exist "C:\Python27-x64\" (
-    SET PY=C:\Python27-x64\python.exe
-    SET PYI=C:\Python27-x64\Scripts\pyinstaller.exe 
-) else (
-    SET PY=C:\Python27\python.exe
-    SET PYI=C:\Python27\Scripts\pyinstaller.exe
-    if not exist "%PY%" goto ERROR
-    if not exist "%PYI%" goto ERROR
-)
+SET PACKAGE=%CD%\loki
+SET DIST=%CD%\dist
+SET PYI=pyinstaller
 
 :: Cleaning all old versions
 RMDIR /S /Q build
 
-ECHO -----------------------------------------
-ECHO LOKI Build Script
-ECHO -----------------------------------------
-ECHO Checking prerequisites and configuration ...
-
-:: Windows 2003 Support
-::
-ECHO Checking for msvcr100.dll in order to provide Windows 2003 support ...
-SET WIN2003=
-if not exist "%SystemRoot%\System32\msvcr100.dll" (
-    echo File %SystemRoot%\System32\msvcr100.dll not found.
-    echo No support for Windows 2003 server systems.
-    echo Download and install https://www.microsoft.com/en-us/download/details.aspx?id=26999 if you need that support
-) else (
-    echo Required file msvcr100.dll found. Windows 2003 will be supported.
-    SET WIN2003=-win2003sup
-)
-
-:: Private Rules
-:: See https://github.com/Neo23x0/Loki/#package-loki-with-a-custom-ruleset for details
-SET PRIVRULES=no
-%PY% loki-package-builder.py --ruledir "%cd%\private-signatures" --target rules
-if !errorlevel! neq 0 GOTO ERROR
-if exist "%cd%\rules" (
-    echo Private signatures directory found. The contents will be encrypted and added to the package.
-    SET PRIVRULES=
-)
-
-:: COMPILATION
-:: LOKI Upgrader
-ECHO Compiling LOKI UPGRADER ...
-%PYI% "%cd%\loki-upgrader%WIN2003%.spec"
-if !errorlevel! neq 0 GOTO ERROR
-:: LOKI
+:: BUILD LOKI
 ECHO Compiling LOKI ...
-%PYI% "%cd%\loki-%PRIVRULES%privrules%WIN2003%.spec"
+%PYI% --noupx loki.spec
 if !errorlevel! neq 0 GOTO ERROR
-ECHO Check the ./dist directory for the compiled executables
-GOTO END
+ECHO Compiling LOKI UPGRADER ...
+%PYI% --noupx loki-upgrader.spec
+if !errorlevel! neq 0 GOTO ERROR
+
+:: Copy Executables and other files to the package folder
+move .\dist\loki.exe %PACKAGE%\
+move .\dist\loki-upgrader.exe %PACKAGE%\
+mkdir %PACKAGE%\tools
+copy .\tools\pe-sieve32.exe %PACKAGE%\tools
+copy .\tools\pe-sieve64.exe %PACKAGE%\tools
+
+:: Delete files
+REM DEL /Q %PACKAGE%\signature-base
+DEL /Q %PACKAGE%\*.log
+DEL /Q %DIST%\loki_%VER%.zip
+IF EXIST %PACKAGE%\signature-base (
+    rmdir %PACKAGE%\signature-base /s /q
+)
 
 :ERROR
-echo "An error occured. The build failed."
+ECHO An error occured. Build interrupted.
 
 :END
-:: CLEANUP
-ECHO Cleaning up ...
-del /f "%cd%\rules"
-del /f "%cd%\rules.key"
 
---
+++ build_sfx.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+REM Building Thor and creating SFX
+
+setlocal enabledelayedexpansion
+
+SET PACKAGE=%CD%\loki
+SET DIST=%CD%\dist
+SET ZIPPER="c:\Program Files\7-Zip\7z.exe"
+
+:: Version
+set /p VER="Enter Version: "
+
+:: Cleaning all old versions
+RMDIR /S /Q build
+
+:: BUILD LOKI
+CALL build.bat
+
+:: Pack
+%ZIPPER% a -tzip -mm=Deflate -mmt=off -mx5 -mfb=32 -mpass=1 -sccUTF-8 -mem=AES256 -w%PACKAGE% %DIST%\loki_%VER%.zip %PACKAGE%
+GOTO END
+
+:ERROR
+ECHO An error occured. Build interrupted.
+
+:END
+
+
+
