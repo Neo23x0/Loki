4aff67ff4348ca053fbe2eae16643346a4bb843c
Florian Roth <venom14@gmail.com>
2021-04-28 08:58:56 +0200

    docs: version number increased

--- lib/lokilogger.py
+++ lib/lokilogger.py
@@ -15,7 +15,7 @@ from logging import handlers
 import socket
 from .helpers import removeNonAsciiDrop
 
-__version__ = '0.40.0'
+__version__ = '0.41.0'
 
 
 # Logger Class -----------------------------------------------------------------
