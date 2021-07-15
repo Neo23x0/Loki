c8f52b8bbdab0b604b8bc71707e85ac30485786c
Florian Roth <florian.roth@nextron-systems.com>
2021-07-15 09:07:35 +0200

    docs: update version number

--- lib/lokilogger.py
+++ lib/lokilogger.py
@@ -15,7 +15,7 @@ from logging import handlers
 import socket
 from .helpers import removeNonAsciiDrop
 
-__version__ = '0.41.1'
+__version__ = '0.41.2'
 
 
 # Logger Class -----------------------------------------------------------------
